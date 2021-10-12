8e157e61f21f4aaa8d37b8a31ebcb61bca68172f
adil <adilahmmed1444@gmail.com>
2021-10-12 09:39:20 +0530

    repeat invitation fixed

--- routes/invite.js
+++ routes/invite.js
@@ -1,18 +1,39 @@
 const express=require('express')
 const route=express.Router()
 const userDB=require('../models/users')
+
+
 route.post('/invite',(req,res)=>{
+    let repeatflag=false
     if(req.body.inviteemail===""){
         return(res.json({status:"error",msg:"email should provide"}))
     }
-    userDB.updateOne({_id:req.user.id},{$push:{invite:req.body.inviteemail}})
-    .then((result)=>{
-        res.json({status:"ok",msg:"invite success"})
-        console.log(result)
-    })
-    .catch((err)=>{
-        res.json({status:"error"})
-    })
+    if(req.body.inviteemail.includes("@")){
+        userDB.find({_id:req.user.id},{invite:{$elemMatch:{$eq:req.body.inviteemail}}})
+            .then((result)=>{
+                console.log(result[0].invite.length)
+                if(result[0].invite.length ===0){
+                    userDB.updateOne({_id:req.user.id},{$push:{invite:req.body.inviteemail}})
+                        .then((result)=>{
+                        res.json({status:"ok",msg:"invite success"})
+                        console.log(result)
+                        })
+                        .catch((err)=>{
+                        res.json({status:"error"})
+                        })
+                }else{
+                    res.json({status:"error",msg:"you can't invite same email"})
+                }               
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+            
+        
+    }else{
+        return(res.json({status:"error",msg:"email should provide"}))
+        console.log("its should be email")
+    }
 })
 
 route.get('/invited',(req,res)=>{
